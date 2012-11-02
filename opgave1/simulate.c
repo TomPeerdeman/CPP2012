8a8dc01b4c0e068a1b29ae9b1ce266e310ec7c8c
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-11-02 17:37:23 +0100

    Locking

--- opgave1/simulate.c
+++ opgave1/simulate.c
@@ -22,13 +22,39 @@ double *gl_old_array;
 double *gl_current_array;
 double *gl_next_array;
 
+int nThreadsUnfinished;
+int numThreads;
+pthread_cond_t threadsDone = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 /* Add any functions you may need (like a worker) here. */
 void *compute(void *p){
 	// bereken A voor gegeven i's -> wachten tot alle threads klaar zijn
 	// --> berekenen A voor gegeven i's voor t+1 --> etc
 	i_range_t *range = (i_range_t *) p;
 	
-	printf("Hello world! Range: %d-%d\n", range->i_min, range->i_max);
+	int t;
+	// TODO: t_max ipv 10
+	for(t = 0; t < 10; t++){
+		printf("Hello world! (%d) Range: %d-%d\n", t, range->i_min, range->i_max);
+		
+		// Calculate Ai_min, t t/m Ai_max, t here
+		
+		pthread_mutex_lock(&lock);
+		nThreadsUnfinished--;
+		if(nThreadsUnfinished == 0){
+			// Signal waiting threads, this t is complete.
+			pthread_cond_broadcast(&threadsDone);
+			// Reset num unfinished threads
+			nThreadsUnfinished = numThreads;
+			
+			//TODO: swap buffers
+		}else{
+			// Wait till all threads have completed this t
+			pthread_cond_wait(&threadsDone, &lock);
+		}
+		pthread_mutex_unlock(&lock);
+	}
 	return NULL;
 }
 
@@ -49,10 +75,10 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 	gl_old_array = old_array;
 	gl_current_array = current_array;
 	gl_next_array = next_array;
+	numThreads = num_threads;
 	
 	int nThread;
 	int iPerThread = i_max / num_threads;
-	(void)(iPerThread);
 	
 	// TODO: vervangen door malloc
 	pthread_t threadIds[num_threads];
@@ -61,6 +87,8 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+	
+	nThreadsUnfinished = num_threads;		
 	
 	for(nThread = 0; nThread < num_threads; nThread++){
 		// Set the i range the thread should calculate
