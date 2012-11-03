3f7b538a60bf93b2c5bcc88c35b9c50e8332563d
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-11-03 20:11:03 +0100

    Starting filter

--- opgave1_2/assign1_2.c
+++ opgave1_2/assign1_2.c
@@ -7,32 +7,35 @@
 #include "filter.h"
 
 int main(int argc, char *argv[]){
-  //pthread_t *threads = NULL;
-  unsigned long long upperbound;
-  unsigned long long counter;
-  
-  /* 
-   * You can give an argument telling the program you only want prime numbers
-   * below a given number. If you dont give this argument, prime numbers will
-   * be calculated until the maximum value for an integer is achieved.
-   */
-  if(argc == 2 && atoi(argv[1])>2){
-    upperbound = atoi(argv[1]);
-  }
-  else{
-    upperbound = LLONG_MAX;
-  }
-  printf("Upperbound for primes: %lld\n",upperbound);
-  // create the first queue
-  queue_t *first_queue = newQueue();
+	//pthread_t *threads = NULL;
+	unsigned long long upperbound;
+	unsigned long long counter;
 
-  /*
-   * add all the rational numbers to the first queue, if the queue is full
-   * the number waits to be added.
-   */
-  for(counter = 2; counter<upperbound; counter++){
-    enqueue(first_queue, counter);
-  }
-  
-  return 0;
+	/* 
+	* You can give an argument telling the program you only want prime numbers
+	* below a given number. If you dont give this argument, prime numbers will
+	* be calculated until the maximum value for an integer is achieved.
+	*/
+	if(argc == 2 && atoi(argv[1])>2){
+		upperbound = atoi(argv[1]);
+	}
+	else{
+		upperbound = LLONG_MAX;
+	}
+	printf("Upperbound for primes: %lld\n",upperbound);
+	// create the first queue
+	queue_t *first_queue = newQueue();
+
+	startFilter(first_queue);
+	printf("Started first filter\n");
+
+	/*
+	* add all the rational numbers to the first queue, if the queue is full
+	* the number waits to be added.
+	*/
+	for(counter = 2; counter<upperbound; counter++){
+		enqueue(first_queue, counter);
+	}
+
+	return 0;
 }
