8f51d457627af784b3e0182ccb5503cdf53a5626
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-11-14 14:42:19 +0100

    Even more indents

--- opgave2/assign2_2/assign2_2.c
+++ opgave2/assign2_2/assign2_2.c
@@ -6,45 +6,45 @@
 #include "broadcast.h"
 
 int main(int argc, char **argv){
-	int rc, my_rank, num_tasks, rootnode;
+  int rc, my_rank, num_tasks, rootnode;
 
-	rc = MPI_Init(&argc, &argv);
-	if(rc != MPI_SUCCESS){
-		fprintf(stderr, "Unable to set up MPI\n");
-		// Abort MPI runtime.
-		MPI_Abort(MPI_COMM_WORLD, rc);
-	}
-	MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	
-	if(argc < 2){
-		printf("No root node given.\n");
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-	
-	if(argc < 3){
-		printf("No message given.\n");
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-	
-	rootnode = atoi(argv[1]);
-	if(rootnode < 0 || rootnode > num_tasks -1){
-		printf("Invalid root node.\n");
-		MPI_Abort(MPI_COMM_WORLD, 1);
-	}
-	
-	if(my_rank != rootnode){
-		char *ptr = malloc(100 * sizeof(char));
-		// Recieve broadcast
-		MYMPI_Bcast(ptr, 100, MPI_CHAR, rootnode, MPI_COMM_WORLD);
-		printf("%d received: %s\n", my_rank, ptr);
-		free(ptr);
-	}else{
-		// Send broadcast
-		printf("%d sending %s\n", my_rank, argv[2]);
-		MYMPI_Bcast(argv[2], strlen(argv[2]) + 1, MPI_CHAR, rootnode,
-			MPI_COMM_WORLD);
-	}
-	
-	MPI_Finalize(); // shutdown MPI
+  rc = MPI_Init(&argc, &argv);
+  if(rc != MPI_SUCCESS){
+    fprintf(stderr, "Unable to set up MPI\n");
+    // Abort MPI runtime.
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  
+  if(argc < 2){
+    printf("No root node given.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  
+  if(argc < 3){
+    printf("No message given.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  
+  rootnode = atoi(argv[1]);
+  if(rootnode < 0 || rootnode > num_tasks -1){
+    printf("Invalid root node.\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  
+  if(my_rank != rootnode){
+    char *ptr = malloc(100 * sizeof(char));
+    // Recieve broadcast
+    MYMPI_Bcast(ptr, 100, MPI_CHAR, rootnode, MPI_COMM_WORLD);
+    printf("%d received: %s\n", my_rank, ptr);
+    free(ptr);
+  }else{
+    // Send broadcast
+    printf("%d sending %s\n", my_rank, argv[2]);
+    MYMPI_Bcast(argv[2], strlen(argv[2]) + 1, MPI_CHAR, rootnode,
+      MPI_COMM_WORLD);
+  }
+  
+  MPI_Finalize(); // shutdown MPI
 }
--- opgave2/assign2_2/broadcast.c
+++ opgave2/assign2_2/broadcast.c
@@ -7,52 +7,56 @@ int broadcast_counter = 0;
 
 int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, 
   MPI_Comm communicator){
-        
-    int num_tasks, my_rank, tag;
+  
+  int num_tasks, my_rank, tag;
 
-    // determine total number of tasks, and my mpi process rank
-    MPI_Comm_size(communicator, &num_tasks);
-    MPI_Comm_rank(communicator, &my_rank);
+  // determine total number of tasks, and my mpi process rank
+  MPI_Comm_size(communicator, &num_tasks);
+  MPI_Comm_rank(communicator, &my_rank);
 
-    // only send if you are the root
-    if(my_rank == root){
-      broadcast_counter++;
-      tag = broadcast_counter;
-      printf("Send tag %d from %d\n", tag, my_rank);
+  // only send if you are the root
+  if(my_rank == root){
+    broadcast_counter++;
+    tag = broadcast_counter;
+    printf("Send tag %d from %d\n", tag, my_rank);
+  
+    // ring structure, last rank connects to first rank and vice versa
+    if(root != num_tasks-1)
+      MPI_Send(buffer, count, datatype, root+1, tag, communicator);
+    else
+      MPI_Send(buffer, count, datatype, 0, tag, communicator);
+    if(root != 0)
+      MPI_Send(buffer, count, datatype, root-1, tag, communicator);
+    else
+      MPI_Send(buffer, count, datatype, num_tasks-1, tag, communicator);
+  }
     
-      // ring structure, last rank connects to first rank and vice versa
-      if(root != num_tasks-1)
-        MPI_Send(buffer, count, datatype, root+1, tag, communicator);
-      else
-        MPI_Send(buffer, count, datatype, 0, tag, communicator);
-      if(root != 0)
-        MPI_Send(buffer, count, datatype, root-1, tag, communicator);
-      else
-        MPI_Send(buffer, count, datatype, num_tasks-1, tag, communicator);
-    }
+  // receive if you are not the root
+  else{
+    MPI_Status status;
     
-    // receive if you are not the root
-    else{
-      MPI_Status status;
-      MPI_Recv(buffer, count, datatype, MPI_ANY_SOURCE, MPI_ANY_TAG,
-        communicator, &status);
-      int sender = status.MPI_SOURCE;
-      tag = status.MPI_TAG;
-      printf("%d received tag %d from %d\n", my_rank, tag, sender);
-      int send_to;
+    // Receive any message.
+    MPI_Recv(buffer, count, datatype, MPI_ANY_SOURCE, MPI_ANY_TAG,
+      communicator, &status);
+    int sender = status.MPI_SOURCE;
+    tag = status.MPI_TAG;
+    printf("%d received tag %d from %d\n", my_rank, tag, sender);
+    int send_to;
     
-      if(tag > broadcast_counter){
-        broadcast_counter = tag;
-        
-        if(sender < my_rank){
-          send_to = (sender + 2) % num_tasks;
-        }else{
-          send_to = (sender - 2 + num_tasks) % num_tasks;
-        }
-        printf("%d resend tag %d to %d\n", my_rank, tag, send_to);
-        MPI_Send(buffer, count, datatype, send_to, tag, communicator);
+    // Only resend if i didn't received it already.
+    if(tag > broadcast_counter){
+      broadcast_counter = tag;
+      
+      // Determine where i should send it to.
+      if(sender < my_rank && !(my_rank == num_tasks - 1 && sender == 0)){
+        send_to = (sender + 2) % num_tasks;
+      }else{
+        send_to = (sender - 2 + num_tasks) % num_tasks;
       }
+      printf("%d resend tag %d to %d\n", my_rank, tag, send_to);
+      MPI_Send(buffer, count, datatype, send_to, tag, communicator);
     }
+  }
   
   return 0;
 }
