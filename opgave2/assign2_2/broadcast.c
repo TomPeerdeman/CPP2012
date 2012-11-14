7374e9d904f2046c00cf2873a1612d952a75c65a
Rene <rene66613@hotmail.com>
2012-11-14 13:28:39 +0100

    comments

--- opgave2/assign2_2/broadcast.c
+++ opgave2/assign2_2/broadcast.c
@@ -5,10 +5,14 @@ int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root,
 				
     int num_tasks, my_rank;
 
+    // determine total number of tasks, and my mpi process rank
     MPI_Comm_size(communicator, &num_tasks);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_rank(communicator, &my_rank);
 
+    // only send if you are the root
     if(my_rank == root){
+    
+      // ring structure, last rank connects to first rank and vice versa
       if(root != num_tasks-1)
         MPI_Send(buffer, count, datatype, root+1, 0, communicator);
       else
@@ -18,6 +22,8 @@ int MYMPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root,
       else
         MPI_Send(buffer, count, datatype, num_tasks-1, 0, communicator);
     }
+    
+    // receive if you are not the root
     else{
       MPI_Recv(buffer, count, datatype, root, 0, communicator, MPI_STATUS_IGNORE);
     }
