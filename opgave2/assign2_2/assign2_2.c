3d40e39d1c778db88f463031f1fea104d9ea9126
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-11-14 13:38:38 +0100

    Main

--- opgave2/assign2_2/assign2_2.c
+++ opgave2/assign2_2/assign2_2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mpi.h"
 
 #include "broadcast.h"
 
 int main(int argc, char **argv){
-	int rc, my_rank, num_tasks;
+	int rc, my_rank, num_tasks, rootnode;
 
 	rc = MPI_Init(&argc, &argv);
 	if(rc != MPI_SUCCESS){
@@ -16,7 +17,31 @@ int main(int argc, char **argv){
 	MPI_Comm_size(MPI_COMM_WORLD, &num_tasks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	
-	// Broadcast here
+	if(argc < 2){
+		printf("No root node given.");
+		MPI_Abort(MPI_COMM_WORLD, rc);
+	}
+	
+	if(argc < 3){
+		printf("No message given.");
+		MPI_Abort(MPI_COMM_WORLD, rc);
+	}
+	
+	// TODO: check error
+	rootnode = atoi(argv[1]);
+	
+	if(my_rank != rootnode){
+		char *ptr = malloc(100 * sizeof(char));
+		// Recieve broadcast
+		MYMPI_Bcast(ptr, 100, MPI_CHAR, rootnode, MPI_COMM_WORLD);
+		printf("%d received: %s\n", my_rank, ptr);
+		free(ptr);
+	}else{
+		// Send broadcast
+		printf("%d sending %s\n", my_rank, argv[2]);
+		MYMPI_Bcast(argv[2], strlen(argv[2]) + 1, MPI_CHAR, rootnode,
+			MPI_COMM_WORLD);
+	}
 	
 	MPI_Finalize(); // shutdown MPI
 }
