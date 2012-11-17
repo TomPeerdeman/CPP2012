e305a714a2c212538819b3122006e0c90840713e
Rene <rene66613@hotmail.com>
2012-11-17 13:46:08 +0100

    start

--- opgave3/assign3_1/simulate.c
+++ opgave3/assign3_1/simulate.c
@@ -37,15 +37,16 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
     # pragma omp parallel for private(t)
     for(t = 0;t < t_max; t++)
     {
-      for(i = range->i_min; i <= range->i_max; i++){
-			  next_array[i] = 2 * current_array[i] - old_array[i]
-				  + SPATIAL_IMPACT * (
-					  ((i > 1) ? current_array[i - 1] : 0) - (
-						  2 * current_array[i] - 
-						  ((i < i_max - 1) ? current_array[i + 1]: 0)
-					  )
-				  );
-		}
+        for(i = range->i_min; i <= range->i_max; i++)
+        {
+			    next_array[i] = 2 * current_array[i] - old_array[i]
+				    + SPATIAL_IMPACT * (
+					    ((i > 1) ? current_array[i - 1] : 0) - (
+						    2 * current_array[i] - 
+						    ((i < i_max - 1) ? current_array[i + 1]: 0)
+					    )
+				    );
+		    }
     }
 
     return current_array;
