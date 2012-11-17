d5ce12d40317e645c4f58618276025255a097de3
Rene <rene66613@hotmail.com>
2012-11-17 13:45:16 +0100

    start

--- opgave3/assign3_1/simulate.c
+++ opgave3/assign3_1/simulate.c
@@ -6,8 +6,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "omp.h"
 
 #include "simulate.h"
+
+#define SPATIAL_IMPACT 0.2 
 
 
 /*
@@ -28,6 +31,22 @@ double *simulate(const int i_max, const int t_max, const int num_threads,
     /*
      * Your implementation should go here.
      */
+    omp_set_num_threads(num_threads);
+    int t, i;
+    
+    # pragma omp parallel for private(t)
+    for(t = 0;t < t_max; t++)
+    {
+      for(i = range->i_min; i <= range->i_max; i++){
+			  next_array[i] = 2 * current_array[i] - old_array[i]
+				  + SPATIAL_IMPACT * (
+					  ((i > 1) ? current_array[i - 1] : 0) - (
+						  2 * current_array[i] - 
+						  ((i < i_max - 1) ? current_array[i + 1]: 0)
+					  )
+				  );
+		}
+    }
 
     return current_array;
 }
