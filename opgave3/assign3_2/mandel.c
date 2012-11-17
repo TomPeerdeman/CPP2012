3bd72100fac4f50b4c57065a3dfe2238cf3d5a8d
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-11-17 15:08:03 +0100

    Mandel parallel

--- opgave3/assign3_2/mandel.c
+++ opgave3/assign3_2/mandel.c
@@ -21,45 +21,25 @@ const double xMax = +0.5;
 const double dxy = 0.005;
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <limits.h>
+#include "omp.h"
 
 #include "timer.h"
 
 int main(void) {
 	timer_start();
 	
+	omp_set_num_threads(8);
+	
     double cx, cy;
     double zx, zy, new_zx;
 	double time;
-    unsigned char n;
     int nx, ny;
-
-    // The Mandelbrot calculation is to iterate the equation
-    // z = z*z + c, where z and c are complex numbers, z is initially
-    // zero, and c is the coordinate of the point being tested. If
-    // the magnitude of z remains less than 2 for ever, then the point
-    // c is in the Mandelbrot set. We write out the number of iterations
-    // before the magnitude of z exceeds 2, or UCHAR_MAX, whichever is
-    // smaller.
-
-    for (cy = yMin; cy < yMax; cy += dxy) {
-        for (cx = xMin; cx < xMax; cx += dxy) {
-            zx = 0.0; 
-            zy = 0.0; 
-            n = 0;
-            while ((zx*zx + zy*zy < 4.0) && (n != UCHAR_MAX)) {
-                new_zx = zx*zx - zy*zy + cx;
-                zy = 2.0*zx*zy + cy;
-	        zx = new_zx;
-	        n++;
-            }
-			// Write doesn't exists?
-            //write (1, &n, sizeof(n)); // Write the result to stdout
-            fwrite (&n, sizeof(n), 1, stdout); // Write the result to stdout
-        }
-    }
-
-    // Now calculate the image dimensions. We use exactly the same
+	int i;
+	unsigned char *buf;
+	
+	// Now calculate the image dimensions. We use exactly the same
     // for loops as above, to guard against any potential rounding errors.
 
     nx = 0;
@@ -70,6 +50,41 @@ int main(void) {
     for (cy = yMin; cy < yMax; cy += dxy) {
         ny++;
     }
+	
+	buf = malloc(nx * sizeof(unsigned char));
+	if(buf == NULL){
+		fprintf(stderr, "Could not allocate enough memory, aborting.\n");
+		return 1;
+	}
+	
+    // The Mandelbrot calculation is to iterate the equation
+    // z = z*z + c, where z and c are complex numbers, z is initially
+    // zero, and c is the coordinate of the point being tested. If
+    // the magnitude of z remains less than 2 for ever, then the point
+    // c is in the Mandelbrot set. We write out the number of iterations
+    // before the magnitude of z exceeds 2, or UCHAR_MAX, whichever is
+    // smaller.
+
+    for (cy = yMin; cy < yMax; cy += dxy) {
+		#pragma omp parallel for private(cx, i, zx, zy)
+        for (i = 0; i < nx; i++) {
+			cx = i * dxy + xMin;
+            zx = 0.0; 
+            zy = 0.0; 
+            buf[i] = 0;
+            while ((zx*zx + zy*zy < 4.0) && (buf[i] != UCHAR_MAX)) {
+                new_zx = zx*zx - zy*zy + cx;
+                zy = 2.0*zx*zy + cy;
+				zx = new_zx;
+				buf[i]++;
+            }
+        }
+		// Write doesn't exists?
+		//write (1, &n, sizeof(n)); // Write the result to stdout
+		fwrite(buf, sizeof(unsigned char), nx, stdout);
+    }
+
+	free(buf);
 
     fprintf (stderr, "To process the image: convert -depth 8 -size %dx%d gray:output out.jpg\n",
 	     nx, ny);
