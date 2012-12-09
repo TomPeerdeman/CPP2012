fd046ce53421cd31809829e132f533ff7079906d
Rene <rene66613@hotmail.com>
2012-12-09 17:36:47 +0100

    update

--- opgave6/assign6_2/assign6_2.c
+++ opgave6/assign6_2/assign6_2.c
@@ -6,24 +6,25 @@
 #include "cuda_max.h"
 
 int main(int argc, char **argv){
+  float* ret;
 	if(argc < 3){
 	  printf("Usage: %s <number of floats in the list> <Threads per block>\n", argv[0]);
 	  return 1;
 	}
-	int input = atoi(argv[1]);
+	int length = atoi(argv[1]);
 	if(input == 0){
 	  printf("The amount of floats in the list must be a number!\n");
 	  return 1;
 	}
 	
 	int tpb = atoi(argv[2]);
-	if(input == 0){
+	if(tpb == 0){
 	  printf("The amount of threads per block must be a number!\n");
 	  return 1;
 	}
 	int block_size = (int) ceil((double) length / (double) tpb);
 	
-  ret = computeWaveCuda(input, block_size, tpb);
+  ret = computeMaxCuda(length, block_size, tpb);
   printf("The maximum value found is: %lf",ret);
   
   return 0;
