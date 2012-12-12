ff13dfc03406d2f41d7f4561196d983d23e20fcd
Rene <rene66613@hotmail.com>
2012-12-12 13:27:31 +0100

    fix

--- opgave6/assign6_2/cuda_max.h
+++ opgave6/assign6_2/cuda_max.h
@@ -1,6 +1,6 @@
 #ifndef CUDA_MAX_H
 #define CUDA_MAX_H
 
-float *computeMaxCuda(int length, int block_size, int tpb, float* list);
+void computeMaxCuda(int length, int block_size, int tpb, float* list);
 
 #endif
